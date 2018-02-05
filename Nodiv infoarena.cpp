3ba2fda7fe5991f418483cf9241f0cf04364d91c
Plungerdz <rusanrazvan@gmail.com>
2018-02-05 11:51:42 +0200

    Update Nodiv infoarena.cpp

--- Nodiv infoarena.cpp
+++ Nodiv infoarena.cpp
@@ -14,8 +14,10 @@ int fact(int x) {
     return x*fact(x-1);
 }
 */
-void printArray(int* v, int n) {
-    for (int i=0; i<n; i++) {
+void printArray(int* v, int n)
+{
+    for (int i=0; i<n; i++)
+    {
         cout<<v[i]<<" ";
     }
 }
@@ -28,22 +30,30 @@ int* next(int* v, int n) {
 }
 */
 
-int fexp(int x, int n){
+int fexp(int x, int n)
+{
     //if n < 0  then return exp_by_squaring(1 / x, -n);
     if (n == 0) return  1;
     else if (n == 1)  return  x ;
     else if (n % 2 == 0)  return fexp(x * x,  n / 2);
     else if (n % 2 == 1) return x * fexp(x * x, (n - 1) / 2);
 }
-int n;
+int n, m, otk;
 int main()
 {
-
-    int aux[n], v[n];
-    for (int i=0; i<n; i++) aux[i]=0;
-    for (int i=0; i<n; i++) {
-        for (int j=0; j<(n-i); j++) {
-                for (int k=0; k<n; k++) {
+    fin>>t;
+    for (int l=0; l<t; l++)
+    {
+        fin>>n;
+        bool br=false;
+        int aux[m], v[m];
+        for (int i=0; i<m; i++) aux[i]=0;
+        for (int i=0; i<m*m; i++)
+        {
+            for (int j=0; j<(m*m-i); j++)
+            {
+                for (int k=0; m*m<n; k++)
+                {
                     v[k]=aux[k];
                 }
                 v[j]=1;
@@ -52,14 +62,30 @@ int main()
                     cout<<v[k]<<" ";
                 }
                 */
-                cout<<endl;
-                if (j==(n-i-1)) {
-                    for (int k=0; k<n; k++) {
+                int s=0;
+                for (int x=0; x<m; x++) {
+                    for (int y=0; y<m; y++) {
+                    s+=v[x+y]*pow(2,x)*pow(3,y);
+                    }
+                }
+                if (s==n) {br=true;
+                otk=m;
+                }
+                if (br) break;
+                if (j==(n-i-1))
+                {
+                    for (int k=0; k<m*m; k++)
+                    {
                         aux[k]=v[k];
                     }
                 }
+              if (br) break;
+            }
+            if (br) break;
         }
+
     }
+
 
 
 }
